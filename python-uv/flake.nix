67291ae0d86411bd90884ccd93f26bf8b7285cd3
ekkekuru2 <ekke@ekke.jp>
2026-04-17 14:01:33 +0900

    python-uv numpyのために共有ライブラリ

--- python-uv/flake.nix
+++ python-uv/flake.nix
@@ -12,6 +12,19 @@
         pkgs = import nixpkgs { inherit system; };
       in {
         devShell = pkgs.mkShell {
+          # numpyは高速化のために共有ライブラリを使いたいのだが、NixOSでは設計上そのままだとそれができない
+          # 事前にNixOSのconfiguration.nixでnix-ldを有効化した上で、LD_LIBRARY_PATHを設定してあげる。
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (
+            with pkgs;
+            [
+              # glibc
+              # glibc.dev
+              # glib
+              # libgcc
+              stdenv.cc.cc
+              zlib
+            ]
+          );
           buildInputs = [
             pkgs.python314
             pkgs.uv
